9f75158d4e5e1185fd0ec297066198c7733594dd
Kajetan Krawczyk <kaitek666@gmail.com>
2023-07-09 18:02:02 +0100

    added cors

--- CoyposServer/Program.cs
+++ CoyposServer/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(o => o.AddDefaultPolicy(
+    policy =>
+    {
+        policy.WithOrigins(
+            "https://smilginp.evolpe.net/",
+            "http://localhost:8080/",
+            "http://localhost:5016/",
+            "https://localhost:7091/",
+            "http://localhost:13966" // IIS
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    }));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +30,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// <> UseCors must be placed after UseRouting and before UseAuthorization. This is to ensure that CORS headers are included in the response for both authorized and unauthorized calls.
+app.UseCors();
+// </>
 
 app.UseAuthorization();
 
