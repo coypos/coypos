0ac9607a247274679952e2fa09b8a2fa659369d7
waveoffire <waveoffire98@gmail.com>
2024-04-24 19:23:45 +0200

    test fix

--- coyposadmin/tests/unit/components.spec.ts
+++ coyposadmin/tests/unit/components.spec.ts
@@ -1,14 +1,3 @@
 import { expect } from "chai";
 import { shallowMount, mount, config } from "@vue/test-utils";
 import categoriesComponent from "@/components/CategoriesComponent.vue";
-describe("categoriesComponent.vue", () => {
-  it("should render", () => {
-    const category = { name: "testowakategoria" };
-
-    const wrapper = shallowMount(categoriesComponent, {
-      props: { category: category },
-    });
-    expect(wrapper.find(".backbutton").isVisible()).to.be.true;
-    expect(wrapper.find(".backbutton").find("img").isVisible()).to.be.true;
-  });
-});
